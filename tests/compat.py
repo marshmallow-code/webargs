cc4a0539a77ec24e4ed34e64e8ffef29c39cb13b
Steven Loria <sloria1@gmail.com>
2014-02-17 23:34:47 -0500

    Update version
    also, import WebargsError in __init__

--- tests/compat.py
+++ tests/compat.py
@@ -4,16 +4,12 @@ import sys
 PY2 = int(sys.version[0]) == 2
 
 if PY2:
-    def b(s):
-        return s
     text_type = unicode
     binary_type = str
     string_types = (str, unicode)
     unicode = unicode
     basestring = basestring
 else:
-    def b(s):
-        return s.encode('latin-1')
     text_type = str
     binary_type = bytes
     string_types = (str,)
--- webargs/__init__.py
+++ webargs/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.0'
+__version__ = '0.2.0-dev'
 __author__ = 'Steven Loria'
 __license__ = "MIT"
 
 
-from webargs.core import Arg, ValidationError
+from webargs.core import Arg, WebargsError, ValidationError
