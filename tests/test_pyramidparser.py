28f37ffadc1135a0709382be9e0aa940e519f0b2
philtay <philtay51@hotmail.com>
2014-12-02 15:02:42 +0100

    Fix Python 3 compatibility

--- tests/test_pyramidparser.py
+++ tests/test_pyramidparser.py
@@ -46,7 +46,7 @@ def testapp():
 
     @parser.use_args({'myfile': Arg(multiple=True)}, targets=('files',))
     def echofile(request, args):
-        return dict((i.filename, i.file.getvalue()) for i in args['myfile'])
+        return dict((i.filename, text_type(i.file.getvalue())) for i in args['myfile'])
 
     @parser.use_args({'myvalue': Arg(int)})
     def foo(request, args):
