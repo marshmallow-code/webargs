6b770cc3f2bce876d7e59c01a1e44505126d6f0e
Steven Loria <sloria1@gmail.com>
2015-03-01 18:32:02 -0500

    Pin flake8 and deps, update setup.cfg

--- webargs/__init__.py
+++ webargs/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
+from webargs.core import Arg, WebargsError, ValidationError, RequiredArgMissingError, Missing
 
 __version__ = '0.10.0'
 __author__ = 'Steven Loria'
 __license__ = 'MIT'
 
-
-from webargs.core import Arg, WebargsError, ValidationError, RequiredArgMissingError, Missing
 
 __all__ = ['Arg', 'WebargsError', 'ValidationError', 'RequiredArgMissingError', 'Missing']
