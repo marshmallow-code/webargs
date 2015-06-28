53b62ed1001f34206aff5eebca7472bb6c195653
Steven Loria <sloria1@gmail.com>
2015-06-28 14:12:27 -0400

    Bump version and update changelog

--- webargs/__init__.py
+++ webargs/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from webargs.core import Arg, WebargsError, ValidationError, RequiredArgMissingError, Missing
 
-__version__ = '0.14.0-dev'
+__version__ = '0.14.0'
 __author__ = 'Steven Loria'
 __license__ = 'MIT'
 
