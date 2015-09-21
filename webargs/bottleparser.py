5b0c07613647fb4cecfa823f188f56d1fe81f466
Steven Loria <sloria1@gmail.com>
2015-09-20 21:14:43 -0400

    Port bottleparser

--- webargs/bottleparser.py
+++ webargs/bottleparser.py
@@ -4,11 +4,11 @@
 Example: ::
 
     from bottle import route, run
-    from webargs import Arg
+    from marshmallow import fields
     from webargs.bottleparser import use_args
 
     hello_args = {
-        'name': Arg(str, default='World')
+        'name': fields.Str(missing='World')
     }
     @route('/', method='GET')
     @use_args(hello_args)
@@ -23,50 +23,49 @@ import logging
 from bottle import request, HTTPError
 
 from webargs import core
-from webargs.core import text_type
 
 logger = logging.getLogger(__name__)
 
 class BottleParser(core.Parser):
     """Bottle.py request argument parser."""
 
-    def parse_querystring(self, req, name, arg):
+    def parse_querystring(self, req, name, field):
         """Pull a querystring value from the request."""
-        return core.get_value(req.query, name, arg.multiple)
+        return core.get_value(req.query, name, core.is_multiple(field))
 
-    def parse_form(self, req, name, arg):
+    def parse_form(self, req, name, field):
         """Pull a form value from the request."""
-        return core.get_value(req.forms, name, arg.multiple)
+        return core.get_value(req.forms, name, core.is_multiple(field))
 
-    def parse_json(self, req, name, arg):
+    def parse_json(self, req, name, field):
         """Pull a json value from the request."""
         try:
-            return core.get_value(req.json, name, arg.multiple)
+            return core.get_value(req.json, name, core.is_multiple(field))
         except (AttributeError, ValueError):
             pass
-        return core.Missing
+        return core.missing
 
-    def parse_headers(self, req, name, arg):
+    def parse_headers(self, req, name, field):
         """Pull a value from the header data."""
-        return core.get_value(req.headers, name, arg.multiple)
+        return core.get_value(req.headers, name, core.is_multiple(field))
 
-    def parse_cookies(self, req, name, arg):
+    def parse_cookies(self, req, name, field):
         """Pull a value from the cookiejar."""
         return req.get_cookie(name)
 
-    def parse_files(self, req, name, arg):
+    def parse_files(self, req, name, field):
         """Pull a file from the request."""
-        return core.get_value(req.files, name, arg.multiple)
+        return core.get_value(req.files, name, core.is_multiple(field))
 
     def handle_error(self, error):
         """Handles errors during parsing. Aborts the current request with a
         400 error.
         """
         logger.error(error)
-        status = getattr(error, 'status_code', 400)
-        data = getattr(error, 'data', {})
-        raise HTTPError(status=status, body=text_type(error),
-                        headers=data.get('headers'), exception=error)
+        status_code = getattr(error, 'status_code', self.DEFAULT_VALIDATION_STATUS)
+        headers = getattr(error, 'headers', {})
+        raise HTTPError(status=status_code, body=error.messages,
+                        headers=headers, exception=error)
 
     def parse(self, argmap, req=None, *args, **kwargs):
         """Parses the request using the given arguments map.
