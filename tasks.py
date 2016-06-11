21b7fda6c925f791acbd210c3463abb3ec4fd173
Steven Loria <sloria1@gmail.com>
2016-06-11 13:13:25 -0400

    Upgrade invoke

--- tasks.py
+++ tasks.py
@@ -9,8 +9,8 @@ docs_dir = 'docs'
 build_dir = os.path.join(docs_dir, '_build')
 
 @task
-def test(coverage=False, browse=False):
-    flake()
+def test(ctx, coverage=False, browse=False):
+    flake(ctx)
     import pytest
     args = []
     if coverage:
@@ -24,7 +24,7 @@ def test(coverage=False, browse=False):
     sys.exit(retcode)
 
 @task
-def flake():
+def flake(ctx):
     """Run flake8 on codebase."""
     cmd = 'flake8 .'
     if sys.version_info < (3, 4, 1):
@@ -40,41 +40,41 @@ def flake():
     run(cmd, echo=True)
 
 @task
-def clean():
+def clean(ctx):
     run("rm -rf build")
     run("rm -rf dist")
     run("rm -rf webargs.egg-info")
-    clean_docs()
+    clean_docs(ctx)
     print("Cleaned up.")
 
 @task
-def readme(browse=False):
+def readme(ctx, browse=False):
     run('rst2html.py README.rst > README.html')
     if browse:
         webbrowser.open_new_tab('README.html')
 
 @task
-def clean_docs():
-    run("rm -rf %s" % build_dir)
+def clean_docs(ctx):
+    run('rm -rf %s' % build_dir)
 
 @task
-def browse_docs():
+def browse_docs(ctx):
     path = os.path.join(build_dir, 'index.html')
     webbrowser.open_new_tab(path)
 
 @task
-def docs(clean=False, browse=False, watch=False):
+def docs(ctx, clean=False, browse=False, watch=False):
     """Build the docs."""
     if clean:
-        clean_docs()
+        clean_docs(ctx)
     run("sphinx-build %s %s" % (docs_dir, build_dir), echo=True)
     if browse:
-        browse_docs()
+        browse_docs(ctx)
     if watch:
-        watch_docs()
+        watch_docs(ctx)
 
 @task
-def watch_docs():
+def watch_docs(ctx):
     """Run build the docs when a file changes."""
     try:
         import sphinx_autobuild  # noqa
@@ -87,9 +87,9 @@ def watch_docs():
         docs_dir, build_dir, 'webargs'), echo=True, pty=True)
 
 @task
-def publish(test=False):
+def publish(ctx, test=False):
     """Publish to the cheeseshop."""
-    clean()
+    clean(ctx)
     if test:
         run('python setup.py register -r test sdist bdist_wheel', echo=True)
         run('twine upload dist/* -r test', echo=True)
