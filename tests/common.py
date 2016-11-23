f0974de50602100d445f69d7765135cc9ebae32c
Steven Loria <sloria1@gmail.com>
2016-11-22 23:19:47 -0500

    Fix handling missing Nested(many=True) args
    Fix #145
    Fix #120
    
    Also make load_from behavior consistent in aiohttpparser

--- tests/common.py
+++ tests/common.py
@@ -151,6 +151,12 @@ class CommonTestCase(object):
         res = testapp.post_json('/echo_nested_many', in_data)
         assert res.json == in_data
 
+    # Regression test for https://github.com/sloria/webargs/issues/120
+    def test_parse_nested_many_missing(self, testapp):
+        in_data = {}
+        res = testapp.post_json('/echo_nested_many', in_data)
+        assert res.json == {}
+
     def test_parse_json_if_no_json(self, testapp):
         res = testapp.post('/echo')
         assert res.json == {'name': 'World'}
