b956d0f693b6c16288c50e67ab6c0f8df588f262
Steven Loria <sloria1@gmail.com>
2019-01-30 19:34:41 -0500

    Bump version and update changelog

--- webargs/__init__.py
+++ webargs/__init__.py
@@ -7,7 +7,7 @@ from marshmallow import validate
 from webargs.core import dict2schema, ValidationError
 from webargs import fields
 
-__version__ = "5.1.1"
+__version__ = "5.1.1.post0"
 __author__ = "Steven Loria"
 __license__ = "MIT"
 
