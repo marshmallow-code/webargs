87f4bb8cdcb607cb4f15ecbda9a3cb50a3fd5319
Steven Loria <sloria1@gmail.com>
2019-09-07 10:19:22 -0400

    Remove unnnecessary __author__ and __license__

--- src/webargs/__init__.py
+++ src/webargs/__init__.py
@@ -11,8 +11,4 @@ from webargs import fields
 
 __version__ = "5.5.0"
 __version_info__ = tuple(LooseVersion(__version__).version)
-__author__ = "Steven Loria"
-__license__ = "MIT"
-
-
 __all__ = ("dict2schema", "ValidationError", "fields", "missing", "validate")
