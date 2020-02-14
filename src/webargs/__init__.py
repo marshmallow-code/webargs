77882e9e9991f6fccab1b4e3006c6f80e9172e5a
Jérôme Lafréchoux <jerome@jolimont.fr>
2020-02-14 21:56:54 +0100

    Bump version and update changelog

--- src/webargs/__init__.py
+++ src/webargs/__init__.py
@@ -8,6 +8,6 @@ from webargs.core import ValidationError
 from webargs.dict2schema import dict2schema
 from webargs import fields
 
-__version__ = "6.0.0b6"
+__version__ = "6.0.0b7"
 __version_info__ = tuple(LooseVersion(__version__).version)
 __all__ = ("dict2schema", "ValidationError", "fields", "missing", "validate")
