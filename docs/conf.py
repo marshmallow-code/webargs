9bd4a586fa417377f0464e6e563f6ea84bf31f40
Gastón Avila <avila.gas@gmail.com>
2020-05-17 22:29:48 -0300

    Fix 1.4.0 docs, use latest theme (#516)
    * Restrict marshmallow version for building docs
    
    * Add latest theme to old docs
    
    * Remove old theme
    
    * Add webargs requirement
    
    * Try insalling as package

--- docs/conf.py
+++ docs/conf.py
@@ -1,56 +1,76 @@
-# -*- coding: utf-8 -*-
 import datetime as dt
 import sys
 import os
+import sphinx_typlog_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
-import webargs
+sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
+import webargs  # noqa
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-    'sphinx_issues',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_issues",
 ]
 
-primary_domain = 'py'
-default_role = 'py:obj'
+primary_domain = "py"
+default_role = "py:obj"
 
-issues_github_path = 'sloria/webargs'
+github_user = "marshmallow-code"
+github_repo = "webargs"
+
+issues_github_path = "{}/{}".format(github_user, github_repo)
 
 intersphinx_mapping = {
-    'python': ('http://python.readthedocs.io/en/latest/', None),
-    'marshmallow': ('http://marshmallow.readthedocs.io/en/latest/', None),
+    "python": ("http://python.readthedocs.io/en/latest/", None),
+    "marshmallow": ("http://marshmallow.readthedocs.io/en/latest/", None),
 }
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
-language = 'en'
+language = "en"
 
 html_domain_indices = False
-templates_path = ['_templates']
-source_suffix = '.rst'
-project = u'webargs'
-copyright = u'2014-{0:Y}'.format(dt.datetime.utcnow())
+source_suffix = ".rst"
+project = "webargs"
+copyright = "2014-{:%Y}, Steven Loria and contributors".format(dt.datetime.utcnow())
 version = release = webargs.__version__
-exclude_patterns = ['_build']
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
 
 # THEME
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['./_themes']
-html_theme = 'lucuma'
-templates_path = ['_templates']
+html_theme = "sphinx_typlog_theme"
+html_theme_path = [sphinx_typlog_theme.get_path()]
+
+html_theme_options = {
+    "color": "#268bd2",
+    "logo_name": "webargs",
+    "description": "Declarative parsing and validation of HTTP request objects.",
+    "github_user": github_user,
+    "github_repo": github_repo,
+}
 
 html_context = {
-    'project': project,
-    'author': 'Steven Loria',
-    'author_url': 'http://stevenloria.com',
-    'github': 'https://github.com/sloria/webargs',
-    'seo_description': 'A friendly library for parsing HTTP request arguments.',
-    'license': 'MIT Licensed',
+    "tidelift_url": (
+        "https://tidelift.com/subscription/pkg/pypi-webargs"
+        "?utm_source=pypi-webargs&utm_medium=referral&utm_campaign=docs"
+    ),
+    "donate_url": "https://opencollective.com/marshmallow",
+}
+
+html_sidebars = {
+    "**": [
+        "logo.html",
+        "github.html",
+        "globaltoc.html",
+        "donate.html",
+        "searchbox.html",
+        "sponsors.html",
+    ]
 }
