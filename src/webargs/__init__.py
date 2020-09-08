f125042023f794960335063a898a3c767f9159ae
Stephen Rosen <sirosen@globus.org>
2020-09-08 17:18:55 +0000

    Bump version for development

--- src/webargs/__init__.py
+++ src/webargs/__init__.py
@@ -8,6 +8,6 @@ from webargs.core import ValidationError
 from webargs.dict2schema import dict2schema
 from webargs import fields
 
-__version__ = "6.1.1"
+__version__ = "7.0.0-dev"
 __version_info__ = tuple(LooseVersion(__version__).version)
 __all__ = ("dict2schema", "ValidationError", "fields", "missing", "validate")
