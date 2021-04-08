739f3db3697fc5ba87f1079878838589f6b81687
Stephen Rosen <sirosen@globus.org>
2021-04-08 19:58:36 +0000

    Bump version and update changelog

--- src/webargs/__init__.py
+++ src/webargs/__init__.py
@@ -7,6 +7,6 @@ from marshmallow import validate
 from webargs.core import ValidationError
 from webargs import fields
 
-__version__ = "7.0.1"
+__version__ = "8.0.0"
 __version_info__ = tuple(LooseVersion(__version__).version)
 __all__ = ("ValidationError", "fields", "missing", "validate")
