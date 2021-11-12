a8d6549c5c8d85622acbf25463c3a9806a8692c5
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2021-11-12 08:10:49 -0500

    fix: distutils LooseVersion deprecation warning

--- src/webargs/__init__.py
+++ src/webargs/__init__.py
@@ -1,4 +1,4 @@
-from distutils.version import LooseVersion
+from packaging.version import Version
 from marshmallow.utils import missing
 
 # Make marshmallow's validation functions importable from webargs
@@ -8,5 +8,8 @@ from webargs.core import ValidationError
 from webargs import fields
 
 __version__ = "8.0.1"
-__version_info__ = tuple(LooseVersion(__version__).version)
+__parsed_version__ = Version(__version__)
+__version_info__ = __parsed_version__.release
+if __parsed_version__.pre:
+    __version_info__ += __parsed_version__.pre
 __all__ = ("ValidationError", "fields", "missing", "validate")
