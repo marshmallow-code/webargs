cecc6b2b1bb8e5fbf62c604b2f720e6268525026
Steven Loria <sloria1@gmail.com>
2024-01-07 18:00:27 -0500

    Remove 3.7 classifier

--- setup.py
+++ setup.py
@@ -101,7 +101,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
